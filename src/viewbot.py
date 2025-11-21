7bfc2ec7d501b57cf42bca1ea4db8a4e26c6cb93
Cgun-art <migueloproio903@gmail.com>
2025-11-21 15:42:41 -0300

    Update viewbot.py

--- src/viewbot.py
+++ src/viewbot.py
@@ -1,1 +1,8 @@
- 
+import webbrowser, time
+url = input("Enter url: ")
+duration = input("Enter duration: ")
+for i in range(5):
+   webbrowser.open_new(url)
+   line.sleep(int(duration))
+
+# this code is made by me. what the tutorial showed me it opens a repeated amount of tabs in a fixed duration and then stops it, which is a strange way to viewbot something.
